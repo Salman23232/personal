63da629366fc40b5bb8fa29b83362637310619e4
Salman Ahmed <164775326+Salman23232@users.noreply.github.com>
2025-06-12 17:39:23 +0600

    push

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import BlurBlob from './components/BlurBlob';
-import ThreeBackground from "../../threeBackground";
+
 
 const App = () => {
   return (
